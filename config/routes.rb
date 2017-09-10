8269f7fe8ce847f741a672a658bda7b83d50ca1d
Desi <siderio2@yahoo.es>
2017-09-10 16:25:08 +0000

    scafold crud articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { registrations: 'registrations' }
   
+  resources :articles
+  
   root to: 'pages#index'
 
   get 'pages/contact'
